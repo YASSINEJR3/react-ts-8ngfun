171745c0de4f4294a352ca3d54d68609a6ec49f8
YASSINEJR3 <100882813+YASSINEJR3@users.noreply.github.com>
2022-10-29 10:01:08 +0100

    useImmer for arrays

--- App.jsx
+++ App.jsx
@@ -146,12 +146,43 @@ export default function Poem() {
     </>
   );
 }*/
+
+/*const initialList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+];*/
+
+/*
 export default function App() {
+  const [list ,updateList ] =useImmer(initialList);
+  const handleClick = (id) => {
+    updateList(draft => {
+      let i = draft.find(a => a.id == id );
+      i.seen = !i.seen;
+    })
+  }
 
   return (
     <div>
       <h1>Hello world </h1>
+      {list.map(l => ( 
+        <>
+          <li>
+            {l.title} : {l.seen?"true":"false"}
+            <button onClick={() => handleClick(l.id)}> Switch </button>
+          </li>
+        </>
+      ))}
     </div>
   )
 
+}*/
+
+export default function App() {
+  return (
+    <div>
+      <h1>HELLO WORLD </h1>
+    </div>
+  );
 }
